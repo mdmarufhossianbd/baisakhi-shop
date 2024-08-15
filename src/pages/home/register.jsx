1cc6c38046519ccf09a50807d28837f9af42f4d6
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-08-15 12:40:44 +0600

    register done with firebase

--- src/pages/home/register.jsx
+++ src/pages/home/register.jsx
@@ -1,13 +1,38 @@
+import { useContext, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 import authImage from "../../assets/auth-page.jpg";
+import { AuthContext } from "../../provider/authProvider";
 const Register = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const {register, updateUserProfile, setReload} = useContext(AuthContext);
+  const navigate = useNavigate()
 
-    const handleRegister = (e) => {
-        e.preventDefault()
-        const name = e.target.name.value;
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(name, email, password);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const photo = "https://avatar.iran.liara.run/public/boy"
+    // password validation
+
+    if (password.length < 6) {
+      return toast.error("Password at least 6 charaters.");
     }
+    register(email, password)
+    .then(result => {
+        updateUserProfile(name, photo, email)
+        .then(()=>{
+            if(result.user){
+            toast.success("Your account Create Successfully")
+            navigate(location.state ? location.state : "/");
+            setReload(true)
+            }
+        })
+    })
+  };
+  
   return (
     <div className="flex min-h-screen border bg-[#f5f8fe]">
       <div className="w-full">
@@ -22,24 +47,60 @@ const Register = () => {
           Welcome to Baishakhi Shop
         </h2>
         <h3 className="text-4xl font-semibold">Register Now</h3>
-        <form onSubmit={handleRegister} className="md:w-1/2 w-full flex flex-col gap-2">
+        <form
+          onSubmit={handleRegister}
+          className="md:w-1/2 w-full flex flex-col gap-2"
+        >
           <div className="flex flex-col w-full">
             <label className="font-medium">Your Name</label>
-            <input className="px-4 py-2 rounded-md focus:outline-none placeholder:text-[#8a8a8a] w-full" type="text" name="name" placeholder="Enter your full name" required />
+            <input
+              className="px-4 py-2 rounded-md focus:outline-none placeholder:text-[#8a8a8a] w-full"
+              type="text"
+              name="name"
+              placeholder="Enter your full name"
+              required
+            />
           </div>
           <div className="flex flex-col w-full">
             <label className="font-medium">Your Email</label>
-            <input className="px-4 py-2 rounded-md focus:outline-none placeholder:text-[#8a8a8a] w-full" type="email" name="email" placeholder="Enter your email address" required />
+            <input
+              className="px-4 py-2 rounded-md focus:outline-none placeholder:text-[#8a8a8a] w-full"
+              type="email"
+              name="email"
+              placeholder="Enter your email address"
+              required
+            />
           </div>
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full relative">
             <label className="font-medium">Password</label>
-            <input className="px-4 py-2 rounded-md focus:outline-none placeholder:text-[#8a8a8a] w-full" type="password" name="password" placeholder="Enter your password" required />
+            <input
+              className="px-4 py-2 rounded-md focus:outline-none placeholder:text-[#8a8a8a] w-full"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Enter your password"
+              required
+            />
+            {showPassword ? (
+              <button onClick={() => setShowPassword(false)}>
+                <IoMdEye className="absolute right-3 top-[55%] hover:cursor-pointer" />
+              </button>
+            ) : (
+              <IoMdEyeOff
+                onClick={() => setShowPassword(true)}
+                className="absolute right-3 top-[55%] hover:cursor-pointer"
+              />
+            )}
           </div>
           <div className="w-full flex justify-center items-center">
-            <input className="w-1/3 bg-[#4195dd] py-2 rounded-md text-white font-medium text-lg hover:cursor-pointer hover:bg-[#3ab5d6] translate duration-200" type="submit" value="Register" />
+            <input
+              className="w-1/3 bg-[#4195dd] py-2 rounded-md text-white font-medium text-lg hover:cursor-pointer hover:bg-[#3ab5d6] translate duration-200"
+              type="submit"
+              value="Register"
+            />
           </div>
         </form>
       </div>
+      <Toaster position="top-right" reverseOrder={false} />
     </div>
   );
 };
---
+++ src/provider/authProvider.jsx
@@ -0,0 +1,75 @@
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import PropTypes from 'prop-types';
+import { createContext, useEffect, useState } from "react";
+import app from '../firebase/firebase.config';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app)
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [reload, setReload] = useState(false)
+    
+    // user register
+    const register = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // update user profile
+    const updateUserProfile = (name, photo, email) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, {
+            displayName : name,
+            email : email,
+            photoURL: photo
+        })
+        .then(()=>{
+            console.log("your account create successfully");
+        })
+        .catch((error)=>{
+            console.log(error.message);
+        })
+    }
+
+    // login
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    
+    // logout
+    const logOut = () => {
+        return signOut(auth)
+    } 
+
+    // user state checking
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unSubscribe()
+        }
+    },[reload])
+    
+    const authInfo = {
+        register,
+        user,
+        loading,
+        login,
+        logOut,
+        setReload,
+        updateUserProfile
+    }
+    return (<AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>);
+};
+
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
+
+
+export default AuthProvider
